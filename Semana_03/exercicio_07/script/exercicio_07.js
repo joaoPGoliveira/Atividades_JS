8c25f3aabc6f74c32236484732091d03b4bcdbf7
joaoPGoliveira <joaopdgo@gmail.com>
2024-03-26 12:27:30 -0300

    Exericicio 07 finalizado

---
+++ Semana_03/exercicio_07/script/exercicio_07.js
@@ -0,0 +1,21 @@
+// O padStart foi utilizado para que o relogio tenha sempre um zero atras quando as horas forem somente uma unidade
+
+function atualizarRelogio() {
+    var dataAtual = new Date();
+    var horas = dataAtual.getHours().toString().padStart(2, '0'); 
+    var minutos = dataAtual.getMinutes().toString().padStart(2, '0');
+    var segundos = dataAtual.getSeconds().toString().padStart(2, '0');
+  
+    var horarioFormatado = horas + ":" + minutos + ":" + segundos;
+
+    document.getElementById('relogio').innerHTML = horarioFormatado;
+  }
+  
+  function iniciarRelogio() {
+    atualizarRelogio();
+    setTimeout(iniciarRelogio, 1000);
+}
+  
+window.onload = function () {
+    iniciarRelogio();
+};

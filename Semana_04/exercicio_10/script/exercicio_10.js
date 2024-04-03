74f91a34c94353cb62bd80785c143d99466ae5ec
joaoPGoliveira <joaopdgo@gmail.com>
2024-04-03 10:52:53 -0300

    Atividade 10

--- Semana_04/exercicio_10/script/exercicio_10.js
+++ Semana_04/exercicio_10/script/exercicio_10.js
@@ -1,83 +1,46 @@
-function Carro() {
-  var marca;
-  var modelo;
-  var ano;
-  var cor;
-  var kilometragem;
-  var valor_fipe;
-
-  // Get e Set da marca
-  this.setMarca = function (Marca) {
-    this.marca = Marca;
-  };
-
-  this.getMarca = function () {
-    return this.marca;
-  };
-
-  // Get e Set do modelo
-  this.setModelo = function (Modelo) {
-    this.modelo = Modelo;
-  };
-
-  this.getModelo = function () {
-    return this.modelo;
-  };
-
-  // Get e Set do ano
-  this.setAno = function (Ano) {
-    this.ano = Ano;
-  };
-
-  this.getAno = function () {
-    return this.ano;
-  };
-
-  // Get e Set da cor
-  this.setCor = function (Cor) {
-    this.cor = Cor;
-  };
-
-  this.getCor = function () {
-    return this.cor;
-  };
-
-  // Get e Set da kilometragem
-  this.setKilometragem = function (Kilometragem) {
-    this.kilometragem = Kilometragem;
-  };
-
-  this.getKilometragem = function () {
-    return this.kilometragem;
-  };
-
-  // Get e Set do valor_fipe
-
-  this.setValorFipe = function (Valor_fipe) {
-    this.valor_fipe = Valor_fipe;
-  };
-
-  this.getValorFipe = function () {
-    return this.valor_fipe;
-  };
-
-  // Método para calcular ano de utilização do veiculo 
-
-  function anosUtilizacao() {
-    var ano_atual = console.log("Qual é o ano atual?");
-    return ano_atual - this.ano;
+class Carro {
+  constructor(marca, modelo, ano, cor, kilometragem, valor_fipe) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano;
+    this.cor = cor;
+    this.kilometragem = kilometragem;
+    this.valor_fipe = valor_fipe;
   }
 
-  // Método para calcular valor do carro
-  function valorMercado() {
-    if (this.kilometragem < 30000) {
-      return this.valor_fipe * 1.1;
-    } else if (this.kilometragem >= 30000 && this.kilometragem <= 50000) {
-      return this.valor_fipe;
-    } else if (this.kilometragem > 50000) {
-      return this.valor_fipe * 0.9;
+  anosUtilizacao() {
+    const anoAtual = new Date().getFullYear();
+    return anoAtual - this.ano;
+  }
+
+  valorMercado() {
+    let valorMercado = 0;
+    if (this.kilometragem <= 30000) {
+      valorMercado = this.valor_fipe * 1.1;
+    } else if (this.kilometragem > 30000 && this.kilometragem <= 50000) {
+      valorMercado = this.valor_fipe;
+    } else {
+      valorMercado = this.valor_fipe * 0.9;
     }
+    return valorMercado.toFixed(2);
   }
 }
 
+function submitForm() {
+  const marca = document.getElementById("marca").value;
+  const modelo = document.getElementById("modelo").value;
+  const ano = parseInt(document.getElementById("ano").value);
+  const cor = document.getElementById("cor").value;
+  const kilometragem = parseInt(document.getElementById("kilometragem").value);
+  const valor_fipe = parseFloat(document.getElementById("valor_fipe").value);
 
+  const carro = new Carro(marca, modelo, ano, cor, kilometragem, valor_fipe);
+
+  const resultDiv = document.getElementById("resultado");
+  resultDiv.innerHTML = `
+    <h2>Informações do Carro:</h2>
+    <p>Valor FIPE: R$ ${carro.valor_fipe.toFixed(2)}</p>
+    <p>Anos de Utilização: ${carro.anosUtilizacao()} anos</p>
+    <p>Valor de Mercado: R$ ${carro.valorMercado()}</p>
+  `;
+}
